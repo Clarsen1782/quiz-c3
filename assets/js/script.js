f4651defda8e176262dff78d6738b12b71ddc35c
Christina Larsen <clarsen1782@outlook.com>
2023-10-02 21:52:28 -0700

    ugh

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var beginButtonE1 = $("#begin");
+var questionsE1 = $("#questions");
+var answerInputE1 = $("#answer");
+var submitButtonE1 = $("#submit");
+
+var isDark = true;
+
+beginButtonE1.on("click", function () {
+    //show first quuestion
+    alert("Hello");
+});
+
+submitButtonE1.on("click", function () {
+    //submit first question and bring up remaining questions
+    alert("lets do a quiz")
+});
+
